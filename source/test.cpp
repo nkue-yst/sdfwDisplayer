f6383b63785eddc9db9b128b8a4ef67524817e62
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2022-02-06 16:38:42 +0900

    Add test code and sdl2

---
+++ source/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello world!" << std::endl;
+    int a;
+    std::cin >> a;
+    std::cout << a << std::endl;
+
+    return 0;
+}
